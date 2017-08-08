452b1694201eedea2f0db8f542c8557309c6efd9
kiddot <kiddo1024@163.com>
2017-08-08 21:22:32 +0800

    init commit teamtask

---
+++ teamtask/src/main/java/com/android/teamtask/debug/TaskApplication.java
@@ -0,0 +1,10 @@
+package com.android.teamtask.debug;
+
+import com.android.common.BaseApplication;
+
+/**
+ * Created by kiddo on 17-8-8.
+ */
+
+public class TaskApplication extends BaseApplication {
+}
---
+++ teamtask/src/main/java/com/android/teamtask/xsadsad.java
@@ -0,0 +1,8 @@
+package com.android.teamtask;
+
+/**
+ * Created by kiddo on 17-8-8.
+ */
+
+public class xsadsad {
+}
