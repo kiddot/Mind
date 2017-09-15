9ef93dc33fbc18596874f416a73333c760f1e8a0
kiddot <kiddo1024@163.com>
2017-09-15 17:13:06 +0800

    add push module

--- common/src/main/java/com/android/common/BaseApplication.java
+++ common/src/main/java/com/android/common/BaseApplication.java
@@ -1,10 +1,24 @@
 package com.android.common;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.android.common.manager.UserManager;
 import com.android.common.network.OkHttpUtils;
 import com.android.common.network.log.LoggerInterceptor;
+import com.android.common.push.InitPush;
+import com.android.common.push.Notifications;
+import com.android.common.push.Push;
+import com.android.tph.client.ClientConfig;
 
 import okhttp3.OkHttpClient;
 
@@ -14,6 +28,8 @@ import okhttp3.OkHttpClient;
  */
 
 public class BaseApplication extends Application {
+    private static final String TAG = "BaseApplication";
+    private static final String IP = "http://192.168.199.133:30000";
     private static BaseApplication mBaseApplication ;
 
     public static BaseApplication getContext(){
@@ -26,6 +42,7 @@ public class BaseApplication extends Application {
         mBaseApplication = this;
         initOkHttp();
         initARouter();
+        initPush();
     }
 
     private void initARouter() {
@@ -43,6 +60,20 @@ public class BaseApplication extends Application {
         clientBuilder.addInterceptor(new LoggerInterceptor("", true));
         OkHttpClient okHttpClient = clientBuilder.build();
         OkHttpUtils.initClient(okHttpClient);
+    }
+
+    private void initPush(){
+        Log.d(TAG, "initPush: ");
+        Notifications.I.init(this.getApplicationContext());
+        Notifications.I.setSmallIcon(R.drawable.fab_add);
+        Notifications.I.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.fab_add));
+        String userName = UserManager.getInstance(this).getUserName();
+        if (userName == null){
+            userName = "kiddo";
+        }
+        InitPush.getInstance().initPush(this, IP, userName);
+        InitPush.getInstance().startPush(this);
+        InitPush.getInstance().bindUser(userName);
     }
 
 }
---
+++ common/src/main/java/com/android/common/push/InitPush.java
@@ -0,0 +1,121 @@
+package com.android.common.push;
+
+import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.common.BuildConfig;
+import com.android.tph.api.Client;
+import com.android.tph.api.ClientListener;
+import com.android.tph.client.ClientConfig;
+
+/**
+ * Created by kiddo on 17-9-15.
+ */
+
+public class InitPush implements ClientListener{
+    private static final String TAG = "InitPush";
+
+    private static InitPush mInitPush = null;
+    private InitPush(){}
+
+    public static InitPush getInstance(){
+        synchronized (InitPush.class){
+               if (mInitPush == null){
+                    mInitPush = new InitPush();
+               }
+        }
+        return mInitPush;
+    }
+
+    /**
+     * 在这里将用户输入的数据，做一层缓存
+     * @param allocServer
+     * @param userId
+     */
+    public void initPush(Context context, String allocServer, String userId) {
+        //公钥由服务端提供和私钥对应
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCghPCWCobG8nTD24juwSVataW7iViRxcTkey/B792VZEhuHjQvA3cAJgx2Lv8GnX8NIoShZtoCg3Cx6ecs+VEPD2fBcg2L4JK7xldGpOJ3ONEAyVsLOttXZtNXvyDZRijiErQALMTorcgi79M5uVX9/jMv2Ggb2XAeZhlLD28fHwIDAQAB";
+
+        String[] address = allocServer.split(":");
+        String serverHost = address[1].substring(2, address[1].length());
+        int serverPort = Integer.parseInt(address[2]);
+        Toast.makeText(context, serverHost + serverPort, Toast.LENGTH_LONG).show();
+
+
+        ClientConfig cc = ClientConfig.build()
+                .setPublicKey(publicKey)
+                .setAllotServer(allocServer)
+                .setServerHost(serverHost)
+                .setServerPort(serverPort)
+                .setDeviceId(getDeviceId(context))
+                .setClientVersion(BuildConfig.VERSION_NAME)
+                .setLogEnabled(BuildConfig.DEBUG)
+                //.setSessionStorageDir(MainActivity.class.getResource("/").getFile())
+                .setEnableHttpProxy(true)
+                .setUserId(userId);
+        Push.I.checkInit(context).setClientConfig(cc);
+        ClientConfig.I.setClientListener(this);
+    }
+
+    public String getDeviceId(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Activity.TELEPHONY_SERVICE);
+        String deviceId = tm.getDeviceId();
+        if (TextUtils.isEmpty(deviceId)) {
+            String time = Long.toString((System.currentTimeMillis() / (1000 * 60 * 60)));
+            deviceId = time + time;
+        }
+        return deviceId;
+    }
+
+    public void startPush(Context context) {
+        Push.I.checkInit(context).startPush();
+        Log.d(TAG, "startPush: ");
+    }
+
+    public void bindUser(String userId) {
+        if (!TextUtils.isEmpty(userId)) {
+            Push.I.bindAccount(userId, "mpush:" + (int) (Math.random() * 10), "alias");
+        }
+    }
+
+    @Override
+    public void onConnected(Client client) {
+
+    }
+
+    @Override
+    public void onDisConnected(Client client) {
+
+    }
+
+    @Override
+    public void onHandshakeOk(Client client, int i) {
+
+    }
+
+    @Override
+    public void onReceivePush(Client client, byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void onKickUser(String s, String s1) {
+
+    }
+
+    @Override
+    public void onBind(boolean b, String s) {
+
+    }
+
+    @Override
+    public void onUnbind(boolean b, String s) {
+
+    }
+}
